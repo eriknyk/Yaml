bf81de499c8716c98dfdcc77dca70cbbbc2a9ab2
Erik Amaru Ortiz <aortiz.erik@gmail.com>
2012-07-06 16:11:13 -0400

    updating sample

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -5,3 +5,4 @@ define('HOME_DIR', realpath(__DIR__ . '/../') . DIRECTORY_SEPARATOR);
 include_once HOME_DIR . 'Yaml.php';
 
 set_include_path(HOME_DIR . 'Tests/Fixtures' . PATH_SEPARATOR . get_include_path());
+
